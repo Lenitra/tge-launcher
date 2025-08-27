e37efe6ba90e20d7e9fa70c0cf1e3aaa28384466
TauMah <lenitramc@gmail.com>
2025-08-27 23:57:51 +0200

    Add initial server packages configuration file
    - Created a new server_packages.sii file to define server package information.
    - Included essential and non-essential DLC lists.
    - Specified map name and dimensions for the server.
    - Added roads data file reference and time compression settings.

---
+++ UpdateServer.bat
@@ -0,0 +1,12 @@
+@REM  configurer le fichier server_config.sii
+@REM Lignes modifiées :
+@REM  - lobby_name: "Ta Gueule Eric"
+@REM  - description: "Serveur prévu pour l'entrprise TGE"
+@REM  - welcome_message: "Amuse toi bien tête de zob"
+@REM  - max_players: xxx #TODO en fonction du launcher
+@REM  - max_vehicles_total: 500
+@REM  - server_logon_token: "C007C91A01481D3700DB5BF27CA4F9A3"
+@REM  - player_damage: false
+@REM  - service_no_collision: true
+@REM  - moderator_list: 1
+@REM  - moderator_list[0]: 76561198410777777
