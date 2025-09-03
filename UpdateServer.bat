4b65664e5e64c7a40a851ff90ffed5b9b940f166
lenitra <lenitramc@gmail.com>
2025-09-03 17:15:04 +0200

    Update player configuration in UpdateServer.bat

--- UpdateServer.bat
+++ UpdateServer.bat
@@ -3,10 +3,13 @@
 @REM  - lobby_name: "Ta Gueule Eric"
 @REM  - description: "Serveur prévu pour l'entrprise TGE"
 @REM  - welcome_message: "Amuse toi bien tête de zob"
-@REM  - max_players: xxx #TODO en fonction du launcher
+@REM  - max_players: 64 #TODO en fonction du launcher
 @REM  - max_vehicles_total: 500
 @REM  - server_logon_token: "C007C91A01481D3700DB5BF27CA4F9A3"
 @REM  - player_damage: false
 @REM  - service_no_collision: true
-@REM  - moderator_list: 1
+@REM  - moderator_list: 4
 @REM  - moderator_list[0]: 76561198410777777
+@REM  - moderator_list[1]: 76561198373615886
+@REM  - moderator_list[2]: 76561199116752696
+@REM  - moderator_list[3]: 76561198938301387
